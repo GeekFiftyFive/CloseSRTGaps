5d5f51b62991fac55be526bd9cc4fd5d3e7e24e1
GeekFiftyFive <op15736@my.bristol.ac.uk>
2020-02-15 07:58:38 +0000

    Add npm module to parse srt file

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const fs = require('fs');
-const util = require('util');
-
-const readFile = util.promisify(fs.readFile);
+const parseSRT = require('parse-srt');
 
 if(process.argv.length < 3) {
     console.error('Usage: node index.js <filename>');
@@ -10,4 +8,5 @@ if(process.argv.length < 3) {
 
 const fileName = process.argv[2];
 
-console.log(fileName);
+let srt = fs.readFileSync(fileName, 'utf-8');
+console.log(parseSRT(srt));
