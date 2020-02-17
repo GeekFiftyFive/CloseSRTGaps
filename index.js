1684bdd3f483cd40f6cff2f017ad4b2703169f9b
GeekFiftyFive <op15736@my.bristol.ac.uk>
2020-02-17 21:32:45 +0000

    Add dependency on electron

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const fs = require('fs');
 const parser = require('subtitles-parser');
+const {app, BrowserWindow} = require('electron');
 
 if(process.argv.length < 3) {
     console.error('Usage: node index.js <filename>');
     process.exit(1);
 }
+
+// Electron
+function createWindow () {
+    // Create the browser window.
+    let win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    })
+  
+    // and load the index.html of the app.
+    win.loadFile('index.html')
+  }
+  
+  app.whenReady().then(createWindow)
 
 const fileName = process.argv[2];
 
